a11bb3bda5adbd780de77b25ece086b8b415ed22
xianxzhang <xianxzhang@wesure.cn>
2018-11-10 15:41:33 +0800

    change code

--- src/web/main.go
+++ src/web/main.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func myHandler(w http.ResponseWriter, r *http.Request) {
+	//解析参数，默认是不会解析的
+	r.ParseForm()
+	log.Println("one request, Method:", r.Method)
+	// 请按照公众平台官网\基本配置中信息填写
+	// token := "webtest"
+	if r.Method == "GET" {
+		for k, v := range r.Form {
+			log.Printf("key=%s, val=%s", k, strings.Join(v, " "))
+
+		}
+	} else if r.Method == "POST" {
+
+	}
+
 	fmt.Fprintf(w, "Hello there!\n")
 }
 
 func main() {
-	http.HandleFunc("/", myHandler) //	设置访问路由
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/wx", myHandler) //	设置访问路由
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
